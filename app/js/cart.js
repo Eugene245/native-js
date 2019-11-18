3bb18789981cf61c85d272c9c352a4ed73bf030f
User 1 <stud@JS1>
2019-11-18 23:28:22 +0700

    added cart

--- app/js/cart.js
+++ app/js/cart.js
@@ -6,23 +6,30 @@ class Cart {
     }
     add(name, price) {
         this.prodArray.push({"name": name, "price": price})
+        return this.prodArray
+    }
+    count(price) {
         this.totalPrice += price
-        return [this.prodArray, this.totalPrice];
+        return this.totalPrice
     }
 }
 
 export default function() {
     
     let newCart = new Cart()
+    let prodTotalPriceSpan = document.createElement("span")
+    prodTotalPriceSpan.style.cssText = "color: #878786; vertical-align: 20%"
+    document.querySelector(".cart-icon").appendChild(prodTotalPriceSpan)
     
     let cartButtonList = document.getElementsByClassName("product-blocks__info-arrow")
     
     for (let i = 0; i < cartButtonList.length; i++) {
         cartButtonList[i].addEventListener("click", e => {
-            let totalCartPrice = newCart.add(
-                e.target.parentElement.parentElement.getElementsByClassName("product-block__info-product-name")[0].innerText,
-                e.target.parentElement.parentElement.getElementsByClassName("product-block__info-cost")[0].innerText)
-                console.log(totalCartPrice[1], Number(totalCartPrice[1].replace("$", "")))
+            let prodName = e.target.parentElement.parentElement.getElementsByClassName("product-block__info-product-name")[0].innerText
+            let prodCost = e.target.parentElement.parentElement.getElementsByClassName("product-block__info-cost")[0].innerText
+            newCart.add(prodName, prodCost)
+            newCart.count(Number(prodCost.replace("$", "")));
+            prodTotalPriceSpan.innerText = `$${newCart.totalPrice}`
         })
     }
     
